57905d81e2410493b8211bfa7e191b22c28f9fb7
TwanPaemeleire <twan.paemeleire@gmail.com>
2025-04-11 16:02:29 +0200

    Added Player Movement.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float _movementSpeed = 10.0f;
+
+    private Vector2 _moveInput;
+
+    private void Update()
     {
-        
+        if (_moveInput == Vector2.zero) return;
+        Vector3 moveDirection = transform.right * _moveInput.x + transform.forward * _moveInput.y;
+        transform.position += moveDirection * _movementSpeed * Time.deltaTime;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnMove(InputAction.CallbackContext context)
     {
-        
+        _moveInput = context.ReadValue<Vector2>();
+        _moveInput.Normalize();
     }
 }
