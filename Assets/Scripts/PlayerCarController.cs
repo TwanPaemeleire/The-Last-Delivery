070a323dc225099ef2de08f00df3a04fb129afaf
TwanPaemeleire <twan.paemeleire@gmail.com>
2025-04-12 13:49:08 +0200

    Added Turning To The Vehicle.

--- Assets/Scripts/PlayerCarController.cs
+++ Assets/Scripts/PlayerCarController.cs
@@ -9,10 +9,10 @@ public class PlayerCarController : MonoBehaviour
     private float _accelerationSpeed  = 20.0f;
     private float _breakSpeed = 15.0f;
     private float _decelerationSpeed = 15.0f;
+    private float _turnSpeed = 90.0f;
     private float _currentSpeed;
     private Rigidbody _rigidbody;
     private Vector2 _moveInput;
-    private Vector3 _lastMoveDirection;
 
     private void Awake()
     {
@@ -22,18 +22,15 @@ public class PlayerCarController : MonoBehaviour
 
     private void Update()
     {
-        // Store Last Move Input So That We Don't Apply 0 Input And Instantly Stop When There's No Input
-        if (_moveInput.y != 0.0f)
-        {
-            _lastMoveDirection = transform.forward * _moveInput.y;
-        }
+        // Rotate The Car
+        transform.Rotate(Vector3.up * _moveInput.x * _turnSpeed * Time.deltaTime);
 
         if (_moveInput.y > 0.0f && _currentSpeed < _maxSpeed) // Holding W -> Accelerate (If Possible)
         {
             _currentSpeed += _accelerationSpeed * Time.deltaTime;
-            if(_currentSpeed > _maxSpeed) _currentSpeed = _maxSpeed;
+            if (_currentSpeed > _maxSpeed) _currentSpeed = _maxSpeed;
         }
-        else if(_moveInput.y < 0.0f && _currentSpeed > _maxReverseSpeed) // Holding S -> Brake
+        else if (_moveInput.y < 0.0f && _currentSpeed > _maxReverseSpeed) // Holding S -> Brake
         {
             _currentSpeed -= _breakSpeed * Time.deltaTime;
             if (_currentSpeed < _maxReverseSpeed) _currentSpeed = _maxReverseSpeed;
@@ -51,8 +48,7 @@ public class PlayerCarController : MonoBehaviour
                 if (_currentSpeed > 0.0f) _currentSpeed = 0.0f;
             }
         }
-        // Sign Flip Because Speed & Input Will Be Negative And Otherwise Cancel Each Other Out And Become Positive Again
-        Vector3 newVelocity = (_lastMoveDirection.y < 0.0f) ? _lastMoveDirection * (-_currentSpeed) : _lastMoveDirection * _currentSpeed;
+        Vector3 newVelocity = transform.forward * _currentSpeed;
         _rigidbody.linearVelocity = newVelocity;
     }
 
@@ -66,7 +62,6 @@ public class PlayerCarController : MonoBehaviour
         _rigidbody.constraints = RigidbodyConstraints.FreezeAll;
         _rigidbody.isKinematic = true;
     }
-
 
     public void OnSteer(InputAction.CallbackContext context)
     {
